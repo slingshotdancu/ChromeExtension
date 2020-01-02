66d622b4780ddc92eda8d9814cc3e5cb92f571d1
Daniel Jobe <slingshotdancu@users.noreply.github.com>
2020-01-02 13:48:30 -0600

    Version 1.2
    First update currently in use

--- background.js
+++ background.js
@@ -1,9 +1,29 @@
 console.log("background script is running!");
-chrome.runtime.onMessage.addListener((msg, sender) => {
-  // First, validate the message's structure.
-  if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
-    // Enable the page-action for the requesting tab.
-    chrome.pageAction.show(sender.tab.id);
-  }
-  return Promise.resolve("Dummy response to keep the console quiet");
+
+//Enables the page action on validation for the selected tab
+chrome.runtime.onMessage.addListener(function (msg, sender) {
+    if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
+        chrome.pageAction.show(sender.tab.id);
+    }
 });
+
+let firstSavedParagraph; 
+let secondSavedParagraph;
+//when the active tab changes, update the paragraphs to appear normalized
+chrome.tabs.onActivated.addListener(updateParagraphs);
+
+//get the active tab and update the textarea if the popup has anything in the textarea
+function updateParagraphs() {
+	chrome.storage.sync.get('pOne', function(obj) {
+//update the current tab's popup
+	chrome.runtime.sendMessage({paragraphOne: obj.pOne});
+		console.log(obj);	
+	});
+
+	chrome.storage.sync.get('pTwo', function(obj) {
+		chrome.runtime.sendMessage({paragraphTwo: obj.pTwo});
+		console.log(obj);	
+	});
+}
+
+
--- popup.js
+++ popup.js
@@ -1,138 +1,254 @@
 console.log("Popup script is running!");
-retrieveButtonState();
+
+
+let testPrefs = {};
+let buttonValue = "Off";
 
 ////////////////////////////////EXAMPLE CODE///////////////////////////////////
 // Once the DOM is ready...
 window.addEventListener('DOMContentLoaded', () => {
-  // ...query for the active tab...
-  chrome.tabs.query({
-    active: true,
-    currentWindow: true
-  }, tabs => {
 
-    // ...and send a request for the DOM info...
-    chrome.tabs.sendMessage(
-        tabs[0].id,
-        {from: 'popup', subject: 'DOMInfo'},
-        // ...also specifying a callback to be called 
-        //    from the receiving end (content script).
-        setDOMInfo);
-  });
+    let turnButton = document.getElementById("turnOn");
+    let calculateButton = document.getElementById("calculate");
+    let clearButton = document.getElementById("clearButton");
+    let infoButton = document.getElementById("infoEntity");
+    let diffOutput = document.getElementById("diffOutput");
+    let textareaOne = document.getElementById("paragraphOne");
+    let textareaTwo = document.getElementById("paragraphTwo");
 
-  // Update the relevant fields with the new data.
-  const setDOMInfo = info => {
-    document.getElementById('total').textContent = info.total;
-  };
-  ////////////////////////////////END OF EXAMPLE CODE////////////////////////////////////
-
-
-let button = document.getElementById("compare");
-if(button !== null) {	  
-	button.addEventListener('click', () => {
-	  chrome.tabs.query({
-	  active: true,
-	  currentWindow: true
-	  }, tabs => {
-	  chrome.tabs.sendMessage(
-	  tabs[0].id,
-	  { from: 'popup', subject: 'compare' },
-	    setParagraphs);
-	  });
-});
-};
-
-const setParagraphs = info => {
-	document.getElementById('compareP').textContent = info.buttons;
-};
-
-let powerState = {};
-let turnButton = document.getElementById("turnOn");
-let clearButton = document.getElementById("clear");
-let resetButton = document.getElementById("reset");
-
-turnButton.addEventListener("click", function() {
-	if(turnButton.style.backgroundColor !== "green") {
-		turnButton.style.backgroundColor = "green";
-		turnButton.textContent = "On";
-		setButtonState("On");
-		chrome.tabs.query({
-		active: true,
-		currentWindow: true
-		}, function(tabs) {
-		chrome.tabs.sendMessage(
-		tabs[0].id,
-		{ from: 'popup', subject: 'buttonOn' },
-		listenForText);
-		});
-	} else if(turnButton.style.backgroundColor === "green") {
-		turnButton.style.backgroundColor = "red";
-		turnButton.textContent = "Off";
-		setButtonState("Off");
-		chrome.tabs.query({
-			active: true,
-			currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(
-			tabs[0].id,
-			{ from: 'popup', subject: 'buttonOff' },
-		stopListeningForText);
-		});
+	chrome.storage.sync.get("pOne", function(obj) {
+	if(obj.pOne) {
+	console.log(obj.pOne);	
+	textareaOne.value = obj.pOne;
+	diffOutput.innerHTML = diffString(
+		textareaOne.value, textareaTwo.value
+		);
+	}
+	});
+	
+	chrome.storage.sync.get("pTwo", function(obj) {
+	if(obj.pTwo) {	
+	console.log(obj.pTwo);	
+	textareaTwo.value = obj.pTwo;
+	diffOutput.innerHTML = diffString(
+		textareaOne.value, textareaTwo.value
+		);
 	}
 	});
 	
 	clearButton.addEventListener("click", function() {
-		chrome.tabs.query({
-		active: true,
-		currentWindow: true
-		}, function(tabs) {
-		chrome.tabs.sendMessage(
-		tabs[0].id,
-		{ from: 'popup', subject: 'clear' });		
+		textareaOne.value = "";
+		textareaTwo.value = "";
+		chrome.storage.sync.set({'pOne': ""}, function() {
+		console.log("Paragraph One storage cleared");
+		});
+		chrome.storage.sync.set({'pTwo': ""}, function() {
+		console.log("Paragraph Two storage cleared");
+		});
+		diffOutput.innerHTML = "";
 	});
-	}); 
+	
+    textareaOne.addEventListener("input", function () {
+        diffOutput.innerHTML = diffString(
+            textareaOne.value, textareaTwo.value
+        );
+		chrome.storage.sync.set({'pOne': textareaOne.value}, function() {
+			console.log("Paragraph One Saved");
+		});
+	});
 
-	resetButton.addEventListener("click", function() {
-		chrome.tabs.query({
-		active: true,
-		currentWindow: true
-		}, function(tabs) {
-		chrome.tabs.sendMessage(
-		tabs[0].id,
-		{ from: 'popup', subject: 'clear' });		
+    textareaTwo.addEventListener("input", function () {
+        diffOutput.innerHTML = diffString(
+            textareaOne.value, textareaTwo.value
+        );
+		chrome.storage.sync.set({'pTwo': textareaTwo.value}, function() {
+			console.log("Paragraph Two Saved");
+		});
 	});
-	});
-
-	let listenForText = textObject => {
-		if(textObject.newText !== undefined) {
-		//alert("It got sent and the copied text is " + textObject.newText);
-		let newP = document.createElement('p');
-		let textNode = document.createTextNode(textObject.newText);
-		newP.appendChild(textNode);
-		let copies = document.getElementById("copies");
-		copies.append(newP);
-		}
-	};
+	
 });
 
 
-	let stopListeningForText = textObject => {
+//Set of Diff Checker functions
+function escape(s) {
+    var n = s;
+    n = n.replace(/&/g, "&amp;");
+    n = n.replace(/</g, "&lt;");
+    n = n.replace(/>/g, "&gt;");
+    n = n.replace(/"/g, "&quot;");
 
-	};
+    return n;
+}
 
-	function setButtonState(buttonValue) {
-	var key = "myKey", testPrefs = {val: buttonValue};
-			chrome.storage.sync.set({key: testPrefs}, function() {
-			console.log("Storage value is set to: " + testPrefs.val);
-			}); 
-		}
+function diffString(o, n) {
+    if (o) {
+        o = o.replace(/\s+$/, '');
+    }
+    n = n.replace(/\s+$/, '');
 
-	function retrieveButtonState() {
-		chrome.storage.sync.get(['key'], function(obj) {
-			console.log(obj.key.val);
-			if(obj.key.val === "On") {
-				console.log("On boot, storage value is " + obj.key.val);	
-				document.getElementById("turnOn").click();
-			} else if(obj.key.val === "Off") {
-				console.log("On boot, storage value is " + obj.key.val);
-			}
-		});
-	}
+    var out = diff(o == "" ? [] : o.split(/\s+/), n == "" ? [] : n.split(/\s+/));
+    var str = "";
+
+    var oSpace = o.match(/\s+/g);
+    if (oSpace == null) {
+        oSpace = ["\n"];
+    } else {
+        oSpace.push("\n");
+    }
+    var nSpace = n.match(/\s+/g);
+    if (nSpace == null) {
+        nSpace = ["\n"];
+    } else {
+        nSpace.push("\n");
+    }
+
+    if (out.n.length == 0) {
+        for (var i = 0; i < out.o.length; i++) {
+            str += '<span class=\"ins\">' + escape(out.o[i]) + oSpace[i] + '</span>';
+        }
+    } else {
+        if (out.n[0].text == null) {
+            for (n = 0; n < out.o.length && out.o[n].text == null; n++) {
+                str += '<span class=\"del\">' + escape(out.o[n]) + oSpace[n] + '</span>';
+            }
+        }
+
+        for (var i = 0; i < out.n.length; i++) {
+            if (out.n[i].text == null) {
+                str += '<span class=\"ins\">' + escape(out.n[i]) + nSpace[i] + '</span>';
+            } else {
+                var pre = "";
+
+                for (n = out.n[i].row + 1; n < out.o.length && out.o[n].text == null; n++) {
+                    pre += '<span class=\"del\">' + escape(out.o[n]) + oSpace[n] + "</span>";
+                }
+                str += " " + out.n[i].text + nSpace[i] + pre;
+            }
+        }
+    }
+
+    return str;
+}
+
+function randomColor() {
+    return "rgb(" + (Math.random() * 100) + "%, " +
+        (Math.random() * 100) + "%, " +
+        (Math.random() * 100) + "%)";
+}
+
+function diffString2(o, n) {
+    o = o.replace(/\s+$/, '');
+    n = n.replace(/\s+$/, '');
+
+    var out = diff(o == "" ? [] : o.split(/\s+/), n == "" ? [] : n.split(/\s+/));
+
+    var oSpace = o.match(/\s+/g);
+    if (oSpace == null) {
+        oSpace = ["\n"];
+    } else {
+        oSpace.push("\n");
+    }
+    var nSpace = n.match(/\s+/g);
+    if (nSpace == null) {
+        nSpace = ["\n"];
+    } else {
+        nSpace.push("\n");
+    }
+
+    var os = "";
+    var colors = new Array();
+    for (var i = 0; i < out.o.length; i++) {
+        colors[i] = randomColor();
+
+        if (out.o[i].text != null) {
+            os += '<span style="background-color: ' + colors[i] + '">' +
+                escape(out.o[i].text) + oSpace[i] + "</span>";
+        } else {
+            os += "<del>" + escape(out.o[i]) + oSpace[i] + "</del>";
+        }
+    }
+
+    var ns = "";
+    for (var i = 0; i < out.n.length; i++) {
+        if (out.n[i].text != null) {
+            ns += '<span style="background-color: ' + colors[out.n[i].row] + '">' +
+                escape(out.n[i].text) + nSpace[i] + "</span>";
+        } else {
+            ns += "<ins>" + escape(out.n[i]) + nSpace[i] + "</ins>";
+        }
+    }
+
+    return {
+        o: os,
+        n: ns
+    };
+}
+
+function diff(o, n) {
+    var ns = new Object();
+    var os = new Object();
+
+    for (var i = 0; i < n.length; i++) {
+        if (ns[n[i]] == null)
+            ns[n[i]] = {
+                rows: new Array(),
+                o: null
+            };
+        ns[n[i]].rows.push(i);
+    }
+
+    for (var i = 0; i < o.length; i++) {
+        if (os[o[i]] == null)
+            os[o[i]] = {
+                rows: new Array(),
+                n: null
+            };
+        os[o[i]].rows.push(i);
+    }
+
+    for (var i in ns) {
+        if (ns[i].rows.length == 1 && typeof (os[i]) != "undefined" && os[i].rows.length == 1) {
+            n[ns[i].rows[0]] = {
+                text: n[ns[i].rows[0]],
+                row: os[i].rows[0]
+            };
+            o[os[i].rows[0]] = {
+                text: o[os[i].rows[0]],
+                row: ns[i].rows[0]
+            };
+        }
+    }
+
+    for (var i = 0; i < n.length - 1; i++) {
+        if (n[i].text != null && n[i + 1].text == null && n[i].row + 1 < o.length && o[n[i].row + 1].text == null &&
+            n[i + 1] == o[n[i].row + 1]) {
+            n[i + 1] = {
+                text: n[i + 1],
+                row: n[i].row + 1
+            };
+            o[n[i].row + 1] = {
+                text: o[n[i].row + 1],
+                row: i + 1
+            };
+        }
+    }
+
+    for (var i = n.length - 1; i > 0; i--) {
+        if (n[i].text != null && n[i - 1].text == null && n[i].row > 0 && o[n[i].row - 1].text == null &&
+            n[i - 1] == o[n[i].row - 1]) {
+            n[i - 1] = {
+                text: n[i - 1],
+                row: n[i].row - 1
+            };
+            o[n[i].row - 1] = {
+                text: o[n[i].row - 1],
+                row: i - 1
+            };
+        }
+    }
+
+    return {
+        o: o,
+        n: n
+    };
+}
